715ecd62b6656681cd23f9f4c4d9a4f652ec26ac
Mjakot <hlebletjaslomaju@gmail.com>
2022-11-28 22:28:27 +0200

    Created Keyboardhook class

---
+++ SlowerMouse/KeyboardHook.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlowerMouse
+{
+    internal class KeyboardHook
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardHookHandler lpfn, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private delegate IntPtr KeyboardHookHandler(int nCode, IntPtr wParam, IntPtr lParam);
+        public delegate void KeyboardHookCallback(Keys key);
+
+        private KeyboardHookHandler hookHandler;
+
+        public event KeyboardHookCallback KeyDown;
+        public event KeyboardHookCallback KeyUp;
+
+        private IntPtr hookID = IntPtr.Zero;
+
+        private const int WM_KEYDOWN = 0x100;
+        private const int WM_SYSKEYDOWN = 0x104;
+        private const int WM_KEYUP = 0x101;
+        private const int WM_SYSKEYUP = 0x105;
+
+        public void Install()
+        {
+            hookHandler = HookFunc;
+            hookID = SetHook(hookHandler);
+        }
+
+        private IntPtr SetHook(KeyboardHookHandler proc)
+        {
+            using (ProcessModule module = Process.GetCurrentProcess().MainModule)
+            {
+                return SetWindowsHookEx(13, proc, GetModuleHandle(module.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookFunc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                int iwParam = wParam.ToInt32();
+
+                if ((iwParam == WM_KEYDOWN || iwParam == WM_SYSKEYDOWN))
+                {
+                    KeyDown?.Invoke((Keys)Marshal.ReadInt32(lParam));
+                }
+
+                if ((iwParam == WM_KEYUP || iwParam == WM_SYSKEYUP))
+                {
+                    KeyUp?.Invoke((Keys)Marshal.ReadInt32(lParam));
+                }
+            }
+
+            return CallNextHookEx(hookID, nCode, wParam, lParam);
+        }
+    }
+}
